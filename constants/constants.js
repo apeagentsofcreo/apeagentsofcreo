b1d70eb73af28052805fce1ee9827120220e0740
5ha2s6 <tech@5ha2s6.com>
2023-11-17 18:19:43 +0000

    minor

--- constants/constants.js
+++ constants/constants.js
@@ -39,7 +39,7 @@ export const heading1 = "Ape Agents"
 export const heading2 = "The Very First Collection Powered and Sponsered by True NFT Technology"
 export const description = "Highly collectible with a feature rich roadmap. rewards, airdrops, VIP passes, giveaways, holder priviledges and on-going activities. Ape Agents marks the beginning of decentralised Metadata."
 export const btnText1 = "Join Discord"
-export const btnText2 = "Mint Your Agent",
+export const btnText2 = "Mint Your Agent"
 
 export const topCard = { 
   name: "Floor Price",
@@ -112,7 +112,7 @@ export const features = [
   {
     reversed: false,
     title: "Ape Agents: Join The Club",
-    description: "THE 1st COLLECTION with TNFT TECHNOLOGY. Highly Collectible & Rewarding AIRDROPS CHALLENGES BONUSES UPGRADES ON-GOING GIVEAWAYS HOLDER PRIVILAGES & MORE TO COME. In the SPIRIT of WEB3 and DECENTRALISATION',
+    description: "THE 1st COLLECTION with TNFT TECHNOLOGY. Highly Collectible & Rewarding AIRDROPS CHALLENGES BONUSES UPGRADES ON-GOING GIVEAWAYS HOLDER PRIVILAGES & MORE TO COME. In the SPIRIT of WEB3 and DECENTRALISATION",
     image: "/images/agentLogo.png",
     imageAlt: "Ape Agent 000",
   },
